df7973a4c6cc7b477b3d4962526537568059c2f3
Lincoln Anderson <landerson@352inc.com>
2018-06-11 01:12:44 -0400

    Sync group data.

--- src/app/pages/Groups.jsx
+++ src/app/pages/Groups.jsx
@@ -1,41 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { map } from 'lodash';
 
-const Groups = () => (
-  <div className="p-content">
-    <h1>Groups</h1>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-    <p>Lorem</p>
-  </div>
-);
+import { base } from 'app/base';
+
+class Groups extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  componentDidMount() {
+    base.bindToState('groups', {
+      context: this,
+      state: 'groups',
+      asArray: true,
+    });
+  }
+  render() {
+    const { groups } = this.state;
+
+    console.log(groups);
+
+    return (
+      <div className="p-content">
+        <h1>Groups</h1>
+        {groups && map(groups, ({ name }) => <p>{ name }</p>)}
+      </div>
+    );
+  }
+}
 
 export default Groups;
