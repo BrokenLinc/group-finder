02e874755120b04f0f6874aa528dec404e902583
Lincoln Anderson <landerson@352inc.com>
2018-06-11 00:55:17 -0400

    Refactor route restriction.

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
-import restrictAccess from 'auth/restrictAccess';
-import ScrollToTop from 'app/components/ScrollToTop';
+import createRestrictedRoute from 'auth/createRestrictedRoute';
+import ScrollToTop from 'components/ScrollToTop';
 import paths from 'app/paths';
 import Menu from 'app/components/Menu';
 import Home from 'app/pages/Home';
 import Groups from 'app/pages/Groups';
 import SignIn from 'app/pages/SignIn';
 
+const RestrictedRoute = createRestrictedRoute(SignIn);
+
 const AppRouter = () => (
   <Router>
     <div className="content-inner">
       <ScrollToTop />
       <Menu />
-      <Route exact path={paths.home()} component={restrictAccess(Home, SignIn)} />
-      <Route exact path={paths.groups()} component={restrictAccess(Groups, SignIn)} />
-      <Route exact path={paths.group(':groupKey')} component={restrictAccess(Home, SignIn)} />
+      <RestrictedRoute exact path={paths.home()} component={Home} />
+      <RestrictedRoute exact path={paths.groups()} component={Groups} />
+      <RestrictedRoute exact path={paths.group(':groupKey')} component={Home} />
     </div>
   </Router>
 );
--- src/auth/RestrictedComponent.js
+++ src/auth/RestrictedComponent.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class RestrictedComponent extends Component {
-  render() {
-    const { ChildComponent, ReplacementComponent, user } = this.props;
+const RestrictedComponent = (props) => {
+  const { ChildComponent, ReplacementComponent, user } = props;
 
-    if (user.isLoading) return null;
+  if (user.isLoading) return null;
 
-    return user.email ? <ChildComponent {...this.props} /> : <ReplacementComponent />;
-  }
-}
+  return user.email ? <ChildComponent {...this.props} /> : <ReplacementComponent />;
+};
+
 RestrictedComponent.propTypes = {
   ChildComponent: PropTypes.func.isRequired,
   ReplacementComponent: PropTypes.func.isRequired,
